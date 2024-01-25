bc0410b0b00c4ec39dab3d323c13c4ed88a0cfe5
maxhof7 <hofmac20@htl-kaindorf.at>
2024-01-25 13:29:18 +0100

    change output

--- src/main/java/at/htlkaindorf/Main.java
+++ src/main/java/at/htlkaindorf/Main.java
@@ -2,6 +2,6 @@ package at.htlkaindorf;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Simon hat an klanen!");
     }
 }
